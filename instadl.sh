d0d821a788220f8ef3fa7bf238c17116df47a95f
Marmathe <berdtecno@gmail.com>
2018-12-25 00:18:35 +0000

    Update instadl.sh

--- instadl.sh
+++ instadl.sh
@@ -9,7 +9,8 @@ if [ "$1" == "-h" ] || [ "$1" == "--help" ] || [ "$1" == "-H" ];then
 	exit 0
 fi
 if [ "$1" == "-u" ] || [ "$1" == "--url" ] || [ "$1" == "-i" ];then
-curl -s --socks5 127.0.0.1:9050 https://www.instagram.com/$2/ | grep -e '"profile_pic_url_hd":' | tr ',' '\n' | grep -e .jpg | grep 's320x320' | awk 'NR==1' | sed 's,", ,g' | awk '{print $3}' | while read url;do $(wget -O $2.jpg $url 2> /dev/null);done
+curl -s --user-agent "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.7.5) Gecko/20060127 Netscap
+e/8.1" https://www.instagram.com/$2/ | grep -e '"profile_pic_url_hd":' | tr ',' '\n' | grep -e .jpg | grep 's320x320' | awk 'NR==1' | sed 's,", ,g' | awk '{print $3}' | while read url;do $(wget -O $2.jpg $url 2> /dev/null);done
 	echo -e "\e[1;31m[~] \e[1;37mSTARTING DOWNLOAD IMAGE"
 	echo -e "\e[1;31m[!] \e[1;37mfinishd \e[1;32m$2.jpg"
 else
